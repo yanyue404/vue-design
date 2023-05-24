0299eb4418d3dc9da2b51834b8d42297c33220c3
RainBow <xiaoyueyue165@gmail.com>
2023-05-25 01:12:32 +0800

    chore: rename

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,11 +7,11 @@ npm run build
 cd docs/.vuepress/dist
 
 git init
-git config user.name 'HcySunYang'
-git config user.email 'HcySunYang@outlook.com'
+git config user.name 'yanyue404'
+git config user.email '1656800216@qq.com'
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:HcySunYang/vue-design.git master:gh-pages
+git push -f git@github.com:yanyue404/vue-design.git master:gh-pages
 
 cd -
